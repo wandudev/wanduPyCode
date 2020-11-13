bdb16357c8e00c8d6faab656092df8fd3a70bf31
wandu <seungyeon.dev@gmail.com>
2020-11-13 18:03:08 +0900

    Initial commit

---
+++ modules/my_area.py
@@ -0,0 +1,6 @@
+PI = 3.14
+def square_area(a):
+    return a ** 2
+def circle_area(r):
+    return PI * r ** 2
+    
---
+++ modules/my_module1.py
@@ -0,0 +1,5 @@
+def func1():
+    print("func1 in my_module1 ")
+def func2():
+    print("func2 in my_module1 ")
+    
